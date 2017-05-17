7c874834d106dd28db63a5e9af58bc93a6a6d34f
masteraaa <masteraaa@imac2.local>
2017-05-17 19:28:17 +0200

    forth

--- tablas/ViewController.swift
+++ tablas/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
 
     @IBOutlet weak var tabla: UITableView!
     
+    // propiedad llamada contador
+    var contador:Int=0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,12 +30,14 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        
+        return 20
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        contador+=1
         let celda=tabla.dequeueReusableCell(withIdentifier: "celda")
-        celda?.textLabel?.text="Hola que tal?"
+        celda?.textLabel?.text="Hola que tal? \(contador*2)"
         return celda!
         
     }
